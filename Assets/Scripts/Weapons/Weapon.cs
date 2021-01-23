7500f3a0758017069e08755272b789ff06959776
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2021-01-23 10:10:27 +0100

    Comportamiento y jerarquía básicos de las armas

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    protected int magazineSize;
+
+    protected IShoot weaponShoot;
+    protected IReload weaponReload;
+}
